75ca6ad8e9722c777c1cfc173ada1deafbfdb986
princemantengu <princemantengu91@gmail.com>
2025-11-28 21:51:59 +0200

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: Ace-Studios-Africa-Hub // GitHub Pages replaces this when publishing
+  base: "/Ace-Studios-Africa-Hub/" // GitHub Pages replaces this when publishing
 });
